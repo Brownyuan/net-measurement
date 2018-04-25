72d7222e6561a1954d2386aee515ddfc7c3a0561
phoenix.lv <phoenix.lv@dorry.io>
2018-04-25 11:32:22 +0800

    remove the array displaying in echo

--- T_PC_ploss.sh
+++ T_PC_ploss.sh
@@ -11,28 +11,28 @@ source ./packetloss.sh
 source ./constants.sh
 
 PC_AP() {
-    greenhint "PACKET LOSS: PC <-----> AP ${AP}"
+    greenhint "PACKET LOSS: PC <-----> AP"
     loop ploss_flow_ping AP
     loop ploss_mtr AP
 }
 
 PC_SR() {
-    greenhint "PACKET LOSS: PC <-----> SR ${SR}"
+    greenhint "PACKET LOSS: PC <-----> SR"
     loop ploss_mtr SR
 }
 
 PC_VPS() {
-    greenhint "PACKET LOSS: PC <-----> VPS ${VPS}"
+    greenhint "PACKET LOSS: PC <-----> VPS"
     loop ploss_mtr VPS
 }
 
 PC_FI() {
-    greenhint "PACKET LOSS: PC <-----> FI ${FI}"
+    greenhint "PACKET LOSS: PC <-----> FI"
     loop ploss_tcp_mtr FI
 }
 
 PC_CI() {
-    greenhint "PACKET LOSS: PC <-----> CI ${CI}"
+    greenhint "PACKET LOSS: PC <-----> CI"
     loop ploss_tcp_mtr CI
 }
 
